4f2388ea81f6053baf88eb02008dd65f192b679c
keremparlak0 <56314081+keremparlak0@users.noreply.github.com>
2023-05-07 17:11:00 +0300

    first

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const fn = function name(params) {
-  console.log(params);
-};
+import express from "express";
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    const photo = {
+        id:1,
+        name: "Photo Name",
+        description: "Photo desc"
+    };
+    res.send(photo);
+})
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('sunucu çalışıyor');
+})
