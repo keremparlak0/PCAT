a83ef8c7fc6bd6ce50ddde62d7aa10c1e3742fd6
keremparlak0 <56314081+keremparlak0@users.noreply.github.com>
2023-05-10 02:30:12 +0300

    mongoose 1

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
 
 //Niddlewares
 app.use(express.static('public'));
 
+//routes
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve( __dirname, "temp/index.html"));
-})
+  res.render('index');
+});
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log('sunucu çalışıyor');
-})
+  console.log('sunucu çalışıyor');
+});
