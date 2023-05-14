5ecdd2acae3943465b62a56c3570bd28d0630b15
keremparlak0 <56314081+keremparlak0@users.noreply.github.com>
2023-05-14 20:53:44 +0300

    model&dynamic item

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const mongoose = require('mongoose');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//Connect DB
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template Engine
 app.set('view engine', 'ejs');
 
-//Niddlewares
+//Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true})); //url'deki datayı okumamızı sağlıyor
+app.use(express.json()); //url'deki datayı json formatına dönüştürüyor
 
 //routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  console.log(photos);
+  res.render('index', {
+    photos: photos
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -23,7 +37,12 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/');
+})
+
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log('sunucu çalışıyor');
+  console.log(`${PORT} portunda çalışıyor. `);
 });
