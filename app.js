da250548d6e3350f799445cbf280c9838ac4166a
keremparlak0 <56314081+keremparlak0@users.noreply.github.com>
2023-05-15 17:50:10 +0300

    UploadImage

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const mongoose = require('mongoose');
 const Photo = require('./models/Photo');
 
@@ -17,15 +19,15 @@ app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true})); //url'deki datayı okumamızı sağlıyor
+app.use(express.urlencoded({ extended: true })); //url'deki datayı okumamızı sağlıyor
 app.use(express.json()); //url'deki datayı json formatına dönüştürüyor
+app.use(fileUpload());
 
 //routes
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({});
-  console.log(photos);
+  const photos = await Photo.find({}).sort('-dateCreated');
   res.render('index', {
-    photos: photos
+    photos: photos,
   });
 });
 
@@ -38,9 +40,30 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
-  res.redirect('/');
-})
+  const uploadDir = 'public/uploads';
+
+  if(!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uplodedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uplodedImage.name;
+
+  uplodedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uplodedImage.name,
+    });
+    res.redirect('/');
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo: photo,
+  });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
